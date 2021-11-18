d1ef211f101e8c373ea534c939a6ad96eb245eae
Siddharth Singh <1906211@kiit.ac.in>
2021-11-18 22:03:27 +0530

    dialog completed

--- app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/ListActivity.kt
+++ app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/ListActivity.kt
@@ -1,21 +1,53 @@
 package com.siddharthsinghbaghel.swipedeletefirebase
 
+import android.app.ActionBar
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
-      
-    
+
+
+
          val fabAdd : FloatingActionButton = findViewById(R.id.fabAdd)
 
          fabAdd.setOnClickListener{
 
+             val customDialog = Dialog(this)
+             customDialog.setContentView(R.layout.dialog_add)
+             customDialog.show()
+             val window: Window? = customDialog.getWindow()
+             window?.setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
+
          }
         
     }
+
+    fun save(view: View) {
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this,ListActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    fun cancel(view: View) {
+        Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this,ListActivity::class.java)
+        startActivity(intent)
+        finish()
+
+
+    }
+
 }
