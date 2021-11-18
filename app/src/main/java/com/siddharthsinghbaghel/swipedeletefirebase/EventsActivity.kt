f2e1aa1d7b9201ca9d15d154f821098d33c73b6b
Siddharth Singh <1906211@kiit.ac.in>
2021-11-18 21:19:15 +0530

    events

--- app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/EventsActivity.kt
+++ app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/EventsActivity.kt
@@ -1,11 +1,21 @@
 package com.siddharthsinghbaghel.swipedeletefirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class EventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
+      
+    
+         val fabAdd : FloatingActionButton = findViewById(R.id.fabAdd)
+
+         fabAdd.setOnClickListener{
+
+         }
+        
     }
 }
