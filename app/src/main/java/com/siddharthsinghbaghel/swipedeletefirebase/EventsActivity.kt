4e150eb05663546d81a001f5a36502ac35a3c4f9
Siddharth Singh <1906211@kiit.ac.in>
2021-11-18 20:26:40 +0530

    welcome activity

---
+++ app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/EventsActivity.kt
@@ -0,0 +1,11 @@
+package com.siddharthsinghbaghel.swipedeletefirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_events)
+    }
+}
--- app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/MainActivity.kt
+++ app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.siddharthsinghbaghel.swipedeletefirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btnStart:Button  = findViewById(R.id.btnWelcome)
+
+
+        btnStart.setOnClickListener{
+
+            val intent = Intent(this,EventsActivity::class.java)
+            startActivity(intent);
+            finish()
+        }
+
+
     }
 }
