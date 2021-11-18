e2ffb883bf43e17c4246515c3a5f1188dae7738d
Siddharth Singh <1906211@kiit.ac.in>
2021-11-18 19:59:35 +0530

    first commit

---
+++ app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.siddharthsinghbaghel.swipedeletefirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
