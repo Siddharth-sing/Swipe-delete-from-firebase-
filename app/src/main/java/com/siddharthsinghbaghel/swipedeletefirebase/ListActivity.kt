17a39e1ba95edebaef0c0b5a6ed74a3d6d5c112c
Siddharth Singh <1906211@kiit.ac.in>
2021-12-03 22:56:27 +0530

    Retriving data

--- app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/ListActivity.kt
+++ app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/ListActivity.kt
@@ -3,14 +3,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 class ListActivity : AppCompatActivity() {
+
+
+    private val db  = FirebaseFirestore.getInstance()
+    lateinit var adapter : ListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
+
+
+        setUpRecyclerView()
 
         val fabAdd : FloatingActionButton = findViewById(R.id.fabAdd)
 
@@ -20,6 +33,31 @@ class ListActivity : AppCompatActivity() {
              startActivity(intent)
          }
         
+    }
+
+    private fun setUpRecyclerView() {
+        Toast.makeText(this,"Here",LENGTH_LONG).show()
+        val query: Query = db.collection("ItemList").orderBy("ts", Query.Direction.DESCENDING)
+        print("Query == $query")
+            val options: FirestoreRecyclerOptions<ItemModel> = FirestoreRecyclerOptions.Builder<ItemModel>()
+            .setQuery(query, ItemModel::class.java)
+            .build()
+
+        adapter = ListAdapter(options)
+        val rv : RecyclerView = findViewById(R.id.rvList)
+        rv.setHasFixedSize(true)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
     }
 
 }
--- app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/ListAdapter.kt
+++ app/src/main/java/com/siddharthsinghbaghel/swipedeletefirebase/ListAdapter.kt
@@ -1,22 +1,35 @@
 package com.siddharthsinghbaghel.swipedeletefirebase
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
-class ListAdapter: FirestoreRecyclerAdapter {
+class ListAdapter(options: FirestoreRecyclerOptions<ItemModel>) :
+    FirestoreRecyclerAdapter<ItemModel, ListAdapter.ListViewHolder>(options) {
 
-    public class ListViewHolder(view:View): RecyclerView.ViewHolder(view){
+     class ListViewHolder(view:View): RecyclerView.ViewHolder(view){
 
         val txtTitle : TextView = view.findViewById(R.id.txtTitle)
         val txtDesc : TextView = view.findViewById(R.id.txtDesc)
+        val txtTs : TextView = view.findViewById(R.id.txtTs)
 
 
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view = ListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false))
+        return view
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, p1: Int, itemModel: ItemModel) {
+        holder.txtTitle.text = itemModel.title
+        holder.txtDesc.text = itemModel.description
+        holder.txtTs.text = itemModel.ts
+    }
 
 
 }
